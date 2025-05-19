5affb75b59ff6732687d0f73090ed256463e58df
h3x-hex <160248326+h3x-hex@users.noreply.github.com>
2025-05-19 18:38:10 +0530

    Update page.tsx

---
+++ app/create/page.tsx
@@ -0,0 +1,12 @@
+import Create from '@/components/create/Create'
+import React from 'react'
+
+const CreatePage = () => {
+  return (
+    <div>
+        <Create/>
+    </div>
+  )
+}
+
+export default CreatePage
